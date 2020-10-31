112fc877bf32d2edd5e401658115b043a6f06e4f
Stefan Kieleithner <stefan.kieleithner@gmail.com>
2020-10-31 15:05:53 +0100

    Add new plant with unique id

--- src/reducers/plants.js
+++ src/reducers/plants.js
@@ -5,8 +5,10 @@ const initialState = {
 const plantReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_PLANT":
+      let newPlant = action.payload;
+      newPlant["id"] = parseInt(state.plants[state.plants.length-1].id)+1;
       return Object.assign({}, state, {
-        plants: [...state.plants, action.payload],
+        plants: [...state.plants, newPlant],
       });
     case "REMOVE_PLANT":
       return state;
